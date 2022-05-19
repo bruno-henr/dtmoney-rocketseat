a29dfb2f44d06f01817b45bc507cef8f23fa7b19
Bruno henrique <brunohenriquex00@gmail.com>
2022-05-18 23:10:31 -0300

    feat(main): utilizando miragejs para criar servico de api fake para o front

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-
+import { createServer } from 'miragejs'
 import { GlobalStyle } from './styles/global'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date(),
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
