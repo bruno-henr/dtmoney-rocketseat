f017f1e93f46283bd816416b4a73ba3bd6fbc9e0
Bruno henrique <brunohenriquex00@gmail.com>
2022-05-18 21:16:48 -0300

    feat(app): importando componente header no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import styled from 'styled-components'
+import { Header } from "./components/Header";
+
 
 export function App() {
   return (
     <div className="App">
-        ola
+      <Header />
     </div>
   );
 }
