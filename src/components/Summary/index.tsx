375f416d1220b5e5a8b2f198fef54c889994dbba
Bruno henrique <brunohenriquex00@gmail.com>
2022-05-18 21:40:28 -0300

    feat(summary): criando componente summary para indicar valores no dashboard

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+//icons
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+// styles
+import { Container } from './styles';
+
+export const Summary: React.FC = () => {
+  return (
+    <Container>
+        <div>
+            <header>
+                <p>Entradas</p>
+                <img src={incomeImg} alt="Entradas" />
+            </header>
+            <strong>R$1000,00</strong>
+        </div>
+
+        <div>
+            <header>
+                <p>Saídas</p>
+                <img src={outcomeImg} alt="Saídas" />
+            </header>
+            <strong>- R$500,00</strong>
+        </div>
+
+        <div className="highlight-background">
+            <header>
+                <p>Total</p>
+                <img src={totalImg} alt="Total" />
+            </header>
+            <strong>R$500,00</strong>
+        </div>
+    </Container>
+  )
+}
