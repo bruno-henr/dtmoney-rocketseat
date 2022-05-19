26c7880ab178a9f44e29a81a587f5b5173d4d64b
Bruno henrique <brunohenriquex00@gmail.com>
2022-05-18 22:42:04 -0300

    feat(dashboard): importando componente de transacoes

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Summary } from '../Summary';
+import { TransactionsTable } from '../TransactionsTable';
 
 import { Container } from './styles'
 
@@ -7,6 +8,7 @@ export const Dashboard: React.FC = () => {
   return (
       <Container >
           <Summary />
+          <TransactionsTable />
       </Container>
   )
 }
