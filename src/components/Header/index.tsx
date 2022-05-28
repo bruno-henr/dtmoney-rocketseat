17ac32277f96ef1b15965d75ac6ad570fdb35fc8
Bruno henrique <brunohenriquex00@gmail.com>
2022-05-28 14:24:33 -0300

    feat(header): obtendo props para abrir modal de adicionar nova transacao

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import logo from '../../assets/logo.svg'
 // styles
 import { Container, Content } from './styles';
 
-export const Header: React.FC = () => {
+type HeaderProps = {
+    onOpenNewTransactionModal: () => void
+}
+export const Header: React.FC<HeaderProps> = ({ onOpenNewTransactionModal }) => {
+   
+
   return (
       <Container>
           <Content>
 
           <img src={logo} alt="dt money" />
-          <button>
+          <button 
+          onClick={onOpenNewTransactionModal}
+          >
               Nova transação
           </button>
           </Content>
+          
       </Container>
   )
 }
